cde44551f806154040b2c3e485ba05c91665a5c8
= <=>
2023-10-14 19:02:10 -0400

    Seperated output into meal and ingredients, now only generates 1 meal at a time

--- DishDive.py
+++ DishDive.py
@@ -6,7 +6,7 @@
 
 # Necessary modules
 import openai                               # pip install openai
-openai.api_key = "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"
+openai.api_key = "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"
 
 from PIL import Image   # pip install pillow
 import requests         # pip install requests
@@ -29,14 +29,17 @@ for food in preferences:
 
 for item in filters:
     filt += item + ", "
-    
+
 for food in allergies:
     aller += food + ", "
-    
-text_prompt = "Imagine you are hungry and need food ideas, give a list of 10 food ideas based on the preferences:" + pref + "with filters: "+ filt + "in the format Meal: Ingredient1, ingredient2 ... where each meal is seperated with a newline and each ingredient is seperated with a comma"
-    
-    
-#print(pref + filt + aller)
+
+
+
+
+text_prompt = "Imagine you are hungry and need food ideas, give a list of ecxactly 1 new food  idea(s) (something not in preferences) based on the preferences:" + pref + "with filters: "+ filt + "in the format \n\n (You Put Meal Name here): Ingredient1, ingredient2 ... \n \nwhere each meal is seperated with a newline and each ingredient is seperated with a comma"
+
+
+
 #print(text_prompt)
 
 
@@ -45,10 +48,24 @@ text_prompt = "Imagine you are hungry and need food ideas, give a list of 10 foo
 chatgpt_response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[{"role": "user", "content": text_prompt}],
-    temperature=0.9,
+    temperature=0.7,
     max_tokens=2000,
     top_p=0.95)
 
 #Get the list of foods
 response = chatgpt_response['choices'][0]['message']['content'].strip()
 print(response)
+
+
+#Seperate into Meal and Ingredients
+
+# Split the response into meal and ingredients
+meal, ingredients_str = response.split(":")
+meal = meal.strip()  # Remove any leading/trailing whitespace
+
+# Split the ingredients into a list
+ingredients = [ingredient.strip() for ingredient in ingredients_str.split(",")]
+
+# Now you have the variables meal and ingredients
+print("Meal:", meal)
+print("Ingredients:", ingredients)
